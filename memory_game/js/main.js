5d7404d20da7587a796505d0f63a5a4ded135ca7
Roberto <saucy.cooking@gmail.com>
2018-10-07 15:17:46 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,15 @@
 // Javascript File
 console.log("Up and Running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = 'king';
-var cardFour = 'king';
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardOne);
 console.log('User flipped ' + cardTwo);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match!');
+	} else alert("Try again you suck...");
+}
