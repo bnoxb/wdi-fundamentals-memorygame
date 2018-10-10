2dca15f559c5c19a14ae3303686cc240bd038b65
Roberto <saucy.cooking@gmail.com>
2018-10-09 18:18:08 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,23 +23,50 @@ var cards = [{
 	cardImage: "images/king-of-diamonds.png"
 	}
 ];
+var score = 0;
 var cardsInPlay = [];
 // function to check if theres a match
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert('You found a match!');
-	} else alert("Try again you suck...");
+		var scoreText = score += 1;
+		document.getElementsByClassName('scoreBoard')[0].innerHTML = "Games Won: " + scoreText;
+		cardsInPlay = [];
+		return;
+	} else alert("Try again.");
+	cardsInPlay = [];
+	return;
 }
 
 //Function to store all the steps that should happen when a user flips a card
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage)
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 }
-flipCard(0);
-flipCard(2);
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+var resetBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.getElementsByTagName('img')[0];
+		cardElement.parentNode.removeChild(cardElement);
+	}
+	createBoard();
+}
+document.getElementsByTagName('button')[0].addEventListener('click', resetBoard);
+createBoard();
